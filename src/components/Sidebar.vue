774a5dbf669e3f4003bb606b645c104263a3c82b
Iskandar-tulkinov <iscandardev@gmail.com>
2024-07-08 21:58:12 +0500

    fix: recommand tailwindcss config

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,12 +1,14 @@
 <script setup></script>
 <template>
-  <div class="bg-black text-red-500">Lorem ipsum dolor sit amet.</div>
-  <div class="flex justify-around">
+  <div class="font-mono bg-black text-red-500 dark:bg-green-500 dark:text-yellow-500">
+    Lorem ipsum dolor sit amet.
+  </div>
+  <div class="flex flex-col justify-start">
     <a href="../../index.html">
       <img src="../assets/img/logo.svg" alt="" />
     </a>
     <i class="icon-moon cursor-pointer active:scale-110"></i>
 
-    <img class="m-12" src="../assets/img/profile-picture.svg" alt="Profile picture " />
+    <img class="m-12" src="../assets/img/profile-picture.svg" width="40" height="40" alt="Profile picture " />
   </div>
 </template>
